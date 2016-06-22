a33917e22cbb8de499518c7bf8d4d4fa1f0f6e87
letrounet <wandjinathan@gmail.com>
2016-06-22 12:55:04 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,3 @@
-
-
-import android.animation.AnimatorSet;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,19 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,8 +18,6 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
     Button voitureArrive, voitureAccepte;
-    private final String voitureAcc = " Votre demande a été acceptée. Votre chauffeur est en chemin. ";
-    private String voitureArr = " Votre chauffeur est là. Informations:  ";
     String nomChauffeur, immatriculation, modele,numero;
     EditText numeroEdittext, nomChauffeurEdittext, immatriculationEditText, modeleEditText;
     Button envoyerSmsButton;
@@ -53,70 +41,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
-        if(v.getId() == R.id.voiture_arrive){
-
-            LayoutInflater inflater = getLayoutInflater();
-            final View dialogView = inflater.inflate(R.layout.popup_chauffeur,null);
-            numeroEdittext = (EditText) dialogView.findViewById(R.id.numero);
-            nomChauffeurEdittext = (EditText) dialogView.findViewById(R.id.chauffeur);
-            immatriculationEditText = (EditText) dialogView.findViewById(R.id.immatriculation);
-            modeleEditText = (EditText) dialogView.findViewById(R.id.modele);
-            AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(this);
-            dialogbuilder.setView(dialogView);
-            dialogbuilder.setPositiveButton(R.string.envoyesms, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                    numero = numeroEdittext.getText().toString();
-                    nomChauffeur = nomChauffeurEdittext.getText().toString();
-                    immatriculation = immatriculationEditText.getText().toString();
-                    modele = modeleEditText.getText().toString();
-
-                    if (!nomChauffeur.trim().isEmpty() && !immatriculation.trim().isEmpty() && !modele.trim().isEmpty() && !numero.trim().isEmpty()) {
-                        voitureArr = voitureArr + " Nom: " + nomChauffeur + ", immat: " + immatriculation + " modele: " + modele;
-                        sendSms(voitureArr, numeroEdittext.getText().toString());
-                    }
-                    dialog.dismiss();
-                }
-            });
-            dialogbuilder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
-            dialogbuilder.create().show();
-
-
-
-        }
-        else if(v.getId() == R.id.course_accepte){
-
-            LayoutInflater inflater = getLayoutInflater();
-            final View dialogView = inflater.inflate(R.layout.popup_accepte,null);
-            // on affiche un boite de dialogue
-            numeroEdittext = (EditText) dialogView.findViewById(R.id.numero);
-            AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(this);
-            dialogbuilder.setView(dialogView);
-            dialogbuilder.setPositiveButton(R.string.envoyesms, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-
-                    sendSms(voitureAcc,numeroEdittext.getText().toString());
-                    dialog.dismiss();
-
-                }
-            });
-            dialogbuilder.setCancelable(true);
-            dialogbuilder.create().show();
-
-        }
-
-    }
-
-    @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        return false;
     }
 
     public void sendSms(String message,String numero){
